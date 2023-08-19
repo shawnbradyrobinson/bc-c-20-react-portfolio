6c5d33d1ac07a243631862ca328dddc74cea18c5
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-08-19 09:20:50 -0500

    fleshed out the projects component of the portfolio

---
+++ my-project/src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        title: "Brew Finder",
+        subtitle: "Vanilla Javascript with Flowbite",
+        description: "A tool that uses the OpenBreweryDB API and Google Maps to locate nearby breweries for the user",
+        image: "../public/brewfinder.jpg",
+        link: "https://shawnbradyrobinson.github.io/brewery-finder/",
+    },
+    {
+        title: "Study Group Maker",
+        subtitle: "Handlebars, Sequelize, Express, bcrypt, Node",
+        description: "An application that hosts study groups, allowing users to create a profile and then search, create, or join study groups on given topics, at varied levels of expertise",
+        image: "../public/studygroupmaker.jpg",
+        link: "https://study-group-maker-61e342b966ed.herokuapp.com/",
+    }
+];
+
+export const skills = [
+    "JavaScript",
+    "React",
+    "Node",
+    "GraphQL",
+    "MySQL",
+    "MongoDB",
+    "Sequelize",
+];
